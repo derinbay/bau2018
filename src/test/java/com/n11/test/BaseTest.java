2d990a5c129b5ebcca9e14a2bd36fd12b8d751dd
Taylan Derinbay <taylan.derinbay@trendyol.com>
2018-12-14 17:03:58 +0300

    change remote url

--- src/test/java/com/n11/test/BaseTest.java
+++ src/test/java/com/n11/test/BaseTest.java
@@ -5,6 +5,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Locale;
 
 public class BaseTest {
@@ -15,13 +20,16 @@ public class BaseTest {
     public void startUp() {
         Locale.setDefault(new Locale("en", "EN"));
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-//        ChromeOptions options = new ChromeOptions();
-//        try {
-//            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
+//        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--incognito");
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         driver.get("https://www.n11.com");
     }
 
